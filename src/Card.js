12e7319f02911cc1db7691ef99bfa9b851a328f0
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-29 14:47:18 -0500

    instal tachyons, add properties id, name, email to card

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 
+
+
 class Card extends React.Component{
   render(){
     return(
-      <div>
+      <div className='bg-light-green dib ma2 br3 pa3 grow bw2 shadow-5'>
         <img src="https://robohash.org/faerwe" alt="robots" />
         <div>
-        <h3>Character Name</h3>
-        <p>andrewlopezcodes@gmail.com</p>
+          <h2>{this.props.name}</h2>
+          <p>{this.props.email}</p>
         </div>        
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card/>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
   </React.StrictMode>
 );
 
