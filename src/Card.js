cefc440671185d918f0351c89bceebcd9b722bcf
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-29 14:01:21 -0500

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Card extends React.Component{
+  render(){
+    return(
+      <div>
+        <img src="" alt="" />
+        <h3>Character Name</h3>
+        <p>andrewlopezcodes@gmail.com</p>
+      </div>
+    );
+  };
+};
+
+export default Card;
