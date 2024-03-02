bde1a9c5cb42e3c83b609803d3982cb6b8b1ca79
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-03-02 08:32:31 -0500

    create scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './app.css';
 
 
@@ -35,8 +36,9 @@ class App extends Component {
         <div className = 'tc'>
           <h1 className='f1'>RobotFriends</h1>
           <SearchBox searchChange={ this.onSearchChange }/>
-          <CardList robots={filteredRobots}/>
-          <br></br>
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+          </Scroll>
         </div>
       );
     }    
