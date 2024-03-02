7b491338ae1d5336499d10ebb22089263fb78d9e
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-03-01 21:15:28 -0500

    remove class from CardList.js

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-
-
-
 class Card extends React.Component{
   render(){
     return(
       <div className='bg-light-green dib ma2 br3 pa3 grow bw2 shadow-5'>
-        <img src="https://robohash.org/faerwe" alt="robots" />
+        <img src={`https://robohash.org/${this.props.id}?200x200`} alt="robots" />
         <div>
           <h2>{this.props.name}</h2>
           <p>{this.props.email}</p>
